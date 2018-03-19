28e0bdc2b1b39c97073c1cbc7a5c16994a2d38f0
Daniel Kapellusch <dkapellusch@users.noreply.github.com>
2018-03-18 21:23:15 -0700

    added some more to auth form

--- src/app/components/auth-page/auth-page.component.ts
+++ src/app/components/auth-page/auth-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from '@common/models/user.model';
+import { Role } from '@common/enums/role.enum';
 
 @Component({
   selector: 'app-auth-page',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-page.component.css']
 })
 export class AuthPageComponent implements OnInit {
+  
+  user = new UserModel("","");
+  roleOptions = Object.keys(Role).slice(4);
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  submit(event:Event) {
+      console.log(Object.keys(Role));
+      event.preventDefault();
+  }
 }
